5ad0b0271daf6ff7243d51e87302d57b1d895160
jjackbauer <ricardovtms@gmail.com>
2023-02-11 00:50:23 -0300

    WIP: add omnirepository structure and runtime endpoint configuration

--- server/server-rdb/Controllers/Authorization.cs
+++ server/server-rdb/Controllers/Authorization.cs
@@ -8,70 +8,19 @@ public class AuthorizationController : ControllerBase
 {
     private readonly Config _config;
     private readonly IBalanceRepository _repository;
+
+    private readonly ICache<AccountBalance> _cache;
+    private readonly BalanceOmniRepository _omniRepository;
     public AuthorizationController(IBalanceRepository repository)
     {
         _config = new Config(CacheStrategy.Fifo, 0);
         _repository = repository;
+        _cache = new FiFo<AccountBalance>(0);
+        _omniRepository = new BalanceOmniRepository(_repository, _config, _cache);
     }
     [HttpPost(Name = "PostTransaction")]
     public async Task<PaymentAuthorizationResponse> PostTransaction(PaymentAuthorizationRequest input){
-        var a = DateTime.Now;
-        var b = DateTime.Now;
-        var balance = new AccountBalance{
-                Amount = 100,
-                UserID = 1
-            };
-
-        List<double> cacheTimeElapses = new List<double>();
-        List<double> dbTimeElapses = new List<double>();
-
-
-        List<AccountBalance> buffer = new List<AccountBalance>();
-
-        buffer.Add(balance);
-        
-        ICache<AccountBalance> cache = new FiFo<AccountBalance>(1000, buffer.ToArray());
-    
-    long start;
-    
-    for(int c =0 ; c< 100; c++)
-    {
-        start = Stopwatch.GetTimestamp();
-            cache.Get(balance.UserID); 
-        cacheTimeElapses.Add(Stopwatch.GetElapsedTime(start).TotalNanoseconds);
-        
-    }
-        
-        
-
-        await _repository.Add(balance);
-
-        await _repository.Commit();
-
-    for(int c = 0; c < 100 ; c++ )
-    {
-        start = Stopwatch.GetTimestamp();
-        await _repository.Get(balance.UserID);
-
-        dbTimeElapses.Add(Stopwatch.GetElapsedTime(start).TotalNanoseconds);
-    }
-        var dbAvg = dbTimeElapses.Average();
-        var cacheAvg = cacheTimeElapses.Average();
-        var cacheisFasterBy = dbAvg/cacheAvg;
-
-        var dbTimeElapsesVar = dbTimeElapses.Average(x => Math.Pow(x - dbAvg, 2));
-
-        var cacheTimeElapsesVar = cacheTimeElapses.Average(x => Math.Pow(x - cacheAvg, 2));
-    
-       PaymentAuthorizationResponse output = new PaymentAuthorizationResponse{
-            Approved = true,
-            CacheHit = true,
-            ArrivalTime = a,
-            DepartureTime = b,
-            TimeElapsedInNanosseconds = (b - a).Nanoseconds,
-        };
-        
-        return output;
+        throw new NotImplementedException();
     }
 
     [HttpGet(Name = "Get Server Configuration")]
