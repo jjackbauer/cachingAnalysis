c297ae90d7362fcd7a51f2da6d57f097555cbb2e
jjackbauer <ricardovtms@gmail.com>
2023-02-16 23:55:07 -0300

    fix command args loader

--- client/caching-client/Utils.cs
+++ client/caching-client/Utils.cs
@@ -4,16 +4,25 @@ public static class Utils
     {
         enviromentVars env = new enviromentVars();
 
-        if(args.Length > 2)
+        /*Console.WriteLine($"args.Length:{args.Length}");
+        foreach (string arg in args)
+            Console.WriteLine($"args[]: {arg}");*/
+
+        if(args.Length == 2)
         {
-            env.Host = args[1];
-            env.Port = Int32.Parse(args[2]);
+            env.Host = args[0];
+            env.Port = Int32.Parse(args[1]);
+
+            //Console.WriteLine("Command Line Configs");
         }else
         {
             env.Host = "localhost";
-            env.Port = 5122;
+            env.Port = 5000;
+
+            //Console.WriteLine("Default Configs");
         }
-        
+
+        Console.WriteLine($"Host:{env.Host} Port:{env.Port}");
         return env;
     }
     public static HttpClient ConfigureHttpClient(enviromentVars env)
